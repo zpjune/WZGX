83cc5bd7312c3894cdab1ffd02324ed9b93a242e
zpjune <1084013577@qq.com>
2019-05-23 13:50:05 +0800

    d

---
+++ src/app_src/api/cangchu/CRKGL/JJCK.js
@@ -0,0 +1,27 @@
+<template>
+    <div id="JJCK">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:"JJCK",
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+
+</style>
+
+
