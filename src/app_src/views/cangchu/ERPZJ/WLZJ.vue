542cce7c0fd1bf57bb7f4777f8396310c0af53c9
tengfei8771 <532050072@qq.com>
2019-05-23 17:18:22 +0800

    1.修改界面

--- src/app_src/views/cangchu/ERPZJ/WLZJ.vue
+++ src/app_src/views/cangchu/ERPZJ/WLZJ.vue
@@ -1,27 +1,109 @@
 <template>
-    <div id="WLZJ">
-
-    </div>
+  <div id="WLZJ" class="app-container calendar-list-container">
+    <el-row style="margin-bottom:10px;">
+      <el-col :xs="5" :sm="5" :md="5" :lg="4" :xl="3">
+        <el-input placeholder="请输入工厂编码" style="width:95%;" size="mini" clearable></el-input>
+      </el-col>
+      <el-col :xs="5" :sm="5" :md="5" :lg="4" :xl="3">
+        <el-button type="primary" icon="el-icon-search" size="mini">查询</el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :xs="12" :sm="12" :md="12" :lg="11" :xl="18">
+        <el-table
+          size="mini"
+          :data="goods"
+          :header-cell-class-name="tableRowClassName"
+          v-loading="listloading"
+          element-loading-text="给我一点时间"
+          border
+          fit
+          highlight-current-row
+          style="width: 100%"
+        >
+          <el-table-column label="工厂代码" prop="BWKEY"></el-table-column>
+          <el-table-column label="大类" prop="MATKL"></el-table-column>
+          <el-table-column label="中类" prop="MATKL1"></el-table-column>
+          <el-table-column label="小类" prop="MATKL2"></el-table-column>
+          <el-table-column label="品名" prop="MATKL3"></el-table-column>
+          <el-table-column label="金额" prop="SALK3"></el-table-column>
+        </el-table>
+        <el-pagination
+          background
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="1"
+          :page-sizes="[10,20,30, 50]"
+          :page-size="1"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="1"
+          style="text-align: center;"
+        ></el-pagination>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"WLZJ",
-    data(){
-        return{
+  name: "WLZJ",
+  data() {
+    return {
+      listloading: false,
+      goods: [
+        {
+          BWKEY: "创智信息技术",
+          MATKL: "服装类",
+          MATKL1: "裙子",
+          MATKL2: "超短裙",
+          MATKL3: "未知类",
+          SALK3: 1000000
+        },
+        {
+          BWKEY: "辽河数码",
+          MATKL: "鞋类",
+          MATKL1: "凉鞋",
+          MATKL2: "露脚凉鞋",
+          MATKL3: "未知类",
+          SALK3: 1000000
+        },
+        {
+          BWKEY: "天津XX贸易公司",
+          MATKL: "上衣",
+          MATKL1: "女士上衣",
+          MATKL2: "女士露肩上衣",
+          MATKL3: "未知类",
+          SALK3: 1000000
+        },
+         {
+          BWKEY: "北京XX贸易公司",
+          MATKL: "裤子",
+          MATKL1: "七分裤",
+          MATKL2: "男士休闲7分库",
+          MATKL3: "未知类",
+          SALK3: 1000000
+        },
+      ]
+    };
+  },
 
-        }
+  methods: {
+    tableRowClassName({ row, rowIndex }) {
+      // 表头行的 className 的回调方法，也可以使用字符串为所有表头行设置一个固定的 className。
+      if (rowIndex === 0) {
+        return "el-button--primary is-active"; // 'warning-row'
+      } // 'el-button--primary is-plain'// 'warning-row'
+      return "";
     },
-    methods:{
-
-    },
-}
+    handleSizeChange() {},
+    handleCurrentChange() {}
+  }
+};
 </script>
 
 
 
 <style lang="scss" scoped>
-
 </style>
 
 
--- src/app_src/views/cangchu/KCGL/SWKC.vue
+++ src/app_src/views/cangchu/KCGL/SWKC.vue
@@ -1,27 +1,93 @@
 <template>
-    <div id="SWKC">
-
-    </div>
+  <div id="SWKC" class="app-container calendar-list-container">
+    <el-row style="margin-bottom:10px;">
+      <el-col :xs="5" :sm="5" :md="5" :lg="4" :xl="3">
+        <el-input placeholder="请输入工厂编码" style="width:95%;" size="mini" clearable></el-input>
+      </el-col>
+      <el-col :xs="5" :sm="5" :md="5" :lg="4" :xl="3">
+        <el-button type="primary" icon="el-icon-search" size="mini">查询</el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :xs="12" :sm="12" :md="12" :lg="11" :xl="12">
+        <el-table
+          size="mini"
+          :data="fac"
+          :header-cell-class-name="tableRowClassName"
+          v-loading="listloading"
+          element-loading-text="给我一点时间"
+          border
+          fit
+          highlight-current-row
+          style="width: 100%"
+        >
+          <el-table-column label="物料编码" prop="MATNR"></el-table-column>
+          <el-table-column label="库存地点" prop="LGORT"></el-table-column>
+          <el-table-column label="数量" prop="GESME"></el-table-column>
+        </el-table>
+        <el-pagination
+          background
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="1"
+          :page-sizes="[10,20,30, 50]"
+          :page-size="1"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="1"
+          style="text-align: center;"
+        ></el-pagination>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"SWKC",
-    data(){
-        return{
-
-        }
+  name: "SWKC",
+  data() {
+    return {
+      listloading: false,
+      fac: [
+        {
+          MATNR: "0101",
+          LGORT: "天津",
+          GESME:800,
+        },
+        {
+          MATNR: "0102",
+          LGORT: "北京",
+          GESME:900,
+        },
+        {
+          MATNR: "0102",
+          LGORT: "上海",
+          GESME:10000,
+        },
+        {
+          MATNR: "0103",
+          LGORT: "深圳",
+          GESME:850,
+        },
+      ]
+    };
+  },
+  methods: {
+    tableRowClassName({ row, rowIndex }) {
+      // 表头行的 className 的回调方法，也可以使用字符串为所有表头行设置一个固定的 className。
+      if (rowIndex === 0) {
+        return "el-button--primary is-active"; // 'warning-row'
+      } // 'el-button--primary is-plain'// 'warning-row'
+      return "";
     },
-    methods:{
-
-    },
-}
+    handleSizeChange() {},
+    handleCurrentChange() {}
+  }
+};
 </script>
 
 
 
 <style lang="scss" scoped>
-
 </style>
 
 
