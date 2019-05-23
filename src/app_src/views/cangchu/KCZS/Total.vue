08238bb958bc0b9364a288028c6e98b883b75ec1
zpjune <1084013577@qq.com>
2019-05-23 17:36:33 +0800

    库存展示

--- src/app_src/views/cangchu/KCZS/Total.vue
+++ src/app_src/views/cangchu/KCZS/Total.vue
@@ -1,27 +1,69 @@
 <template>
-    <div id="total">
-
-    </div>
+  <div id="total" class="app-container calendar-list-container">
+    <el-row>
+      <el-col :span="8">
+        <div class="dpic">
+          <img class="ho" @click="zhongxinkuclick" src="../../../img/zhongxinku.png">
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="dpic">
+          <img class="ho" @click="yuanlianhuaclick" src="../../../img/yuanlianhua.png">
+        </div>
+      </el-col>
+       <el-col :span="8">
+        <div class="dpic">
+          <img class="ho" src="../../../img/gangdong.png">
+        </div>
+      </el-col>
+    </el-row>
+    <div style="height:15px"></div>
+    <el-row>
+    <el-col :span="8">
+        <div class="dpic">
+          <img class="ho" src="../../../img/zhuanyunku.png">
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="dpic">
+          <img class="ho" src="../../../img/zhuanyongguan.png">
+        </div>
+      </el-col>
+       <el-col :span="8">
+        <div class="dpic">
+          
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"toral",
-    data(){
-        return{
-
-        }
-    },
-    methods:{
-
-    },
-}
+  name: "toral",
+  data() {
+    return {};
+  },
+  methods: {
+      zhongxinkuclick(){
+         this.$router.push({path:'/cangchu/KCZS/ZXK'});
+      },
+       yuanlianhuaclick(){
+         this.$router.push({path:'/cangchu/KCZS/YLH'});
+      }
+  }
+};
 </script>
 
 
 
 <style lang="scss" scoped>
-
+.dpic{
+    text-align: center;
+}
+.ho{
+    cursor:pointer;
+}
 </style>
 
 
