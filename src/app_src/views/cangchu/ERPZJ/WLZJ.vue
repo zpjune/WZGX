17575c0930d448e53f67464d6b7c7bbce2e33f69
tengfei8771 <532050072@qq.com>
2019-05-27 15:49:03 +0800

    1.修改无聊查询

--- src/app_src/views/cangchu/ERPZJ/WLZJ.vue
+++ src/app_src/views/cangchu/ERPZJ/WLZJ.vue
@@ -11,7 +11,7 @@
     </el-row>
     <el-row>
       <el-col :xs="24" :sm="20" :md="20" :lg="20" :xl="18">
-        <el-table
+        <!-- <el-table
           size="mini"
           :data="goods"
           :header-cell-class-name="tableRowClassName"
@@ -21,16 +21,35 @@
           fit
           highlight-current-row
           style="width: 100%"
+          row-key="MATKL"
+          lazy
+          :load="load"
         >
-          <!-- <el-table-column label="工厂代码" prop="BWKEY"></el-table-column> -->
-          <el-table-column type="expand">
-          </el-table-column>
+          <el-table-column label="工厂代码" prop="BWKEY"></el-table-column>
           <el-table-column label="大类" prop="MATKL"></el-table-column>
           <el-table-column label="中类" prop="MATKL1"></el-table-column>
           <el-table-column label="小类" prop="MATKL2"></el-table-column>
           <el-table-column label="品名" prop="MATKL3"></el-table-column>
           <el-table-column label="金额" prop="SALK3"></el-table-column>
+        </el-table>-->
+        <el-table
+          :data="goods"
+          style="width: 100%"
+          row-key="id"
+          border
+          size="mini"
+          :header-cell-class-name="tableRowClassName"
+          v-loading="listloading"
+          element-loading-text="给我一点时间"
+          fit
+          highlight-current-row
+        >
+          <!-- <el-table-column label="大类" prop="MATKL"></el-table-column> -->
+          <el-table-column label="品名" prop="MATKL3"></el-table-column>
+          <el-table-column label="编码" prop="CODE"></el-table-column>
+          <el-table-column label="金额" prop="SALK3"></el-table-column>
         </el-table>
+
         <el-pagination
           background
           @size-change="handleSizeChange"
@@ -55,35 +74,133 @@ export default {
       listloading: false,
       goods: [
         {
-          BWKEY: "创智信息技术",
-          MATKL: "服装类",
-          MATKL1: "裙子",
-          MATKL2: "超短裙",
-          MATKL3: "未知类",
+          id: 1,
+          MATKL3: "冶金原料及铸铁管",
+          CODE: "01",
+          SALK3: 1000000,
+          children: [
+            {
+              id: 11,
+              MATKL3: "黑色金属矿采选产品",
+              SALK3: 1000000,
+              CODE: "0101",
+              children: [
+                {
+                  id: 111,
+                  MATKL3: "铁矿石",
+                  SALK3: 1000000,
+                  CODE: "010101",
+                  children: [
+                    {
+                      id: 1111,
+                      MATKL3: "赤铁矿",
+                      SALK3: 1000000,
+                      CODE: "01010101"
+                    },
+                    {
+                      id: 1112,
+                      MATKL3: "褐铁矿",
+                      SALK3: 1000000,
+                      CODE: "01010102"
+                    },
+                    {
+                      id: 1113,
+                      MATKL3: "磁铁矿",
+                      SALK3: 1000000,
+                      CODE: "01010103"
+                    },
+                    {
+                      id: 1114,
+                      MATKL3: "菱铁矿",
+                      SALK3: 1000000,
+                      CODE: "01010104"
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              id: 12,
+              MATKL3: "有色金属矿采选产品",
+              SALK3: 1000000,
+              CODE: "0102",
+              children: [
+                {
+                  id: 121,
+                  MATKL3: "铝矿",
+                  SALK3: 1000000,
+                  CODE: "010202",
+                  children: [
+                    {
+                      id: 1211,
+                      MATKL3: "铝土矿",
+                      SALK3: 1000000,
+                      CODE: "01020201"
+                    },
+                    {
+                      id: 1212,
+                      MATKL3: "铝选矿",
+                      SALK3: 1000000,
+                      CODE: "01020202"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 2,
+          MATKL3: "石油专用管材",
+          CODE: "02",
           SALK3: 1000000
         },
         {
-          BWKEY: "辽河数码",
-          MATKL: "鞋类",
-          MATKL1: "凉鞋",
-          MATKL2: "露脚凉鞋",
-          MATKL3: "未知类",
+          id: 3,
+          MATKL3: "普通钢材",
+          CODE: "03",
           SALK3: 1000000
         },
         {
-          BWKEY: "天津XX贸易公司",
-          MATKL: "上衣",
-          MATKL1: "女士上衣",
-          MATKL2: "女士露肩上衣",
-          MATKL3: "未知类",
+          id: 4,
+          MATKL3: "金属丝、金属绳",
+          CODE: "04",
           SALK3: 1000000
         },
         {
-          BWKEY: "北京XX贸易公司",
-          MATKL: "裤子",
-          MATKL1: "七分裤",
-          MATKL2: "男士休闲7分库",
-          MATKL3: "未知类",
+          id: 5,
+          MATKL3: "有色金属及加工材",
+          CODE: "05",
+          SALK3: 1000000
+        },
+        {
+          id: 6,
+          MATKL3: "建筑五金",
+          CODE: "06",
+          SALK3: 1000000
+        },
+        {
+          id: 7,
+          MATKL3: "石油及产品",
+          CODE: "07",
+          SALK3: 1000000
+        },
+        {
+          id: 8,
+          MATKL3: "煤炭",
+          CODE: "08",
+          SALK3: 1000000
+        },
+        {
+          id: 9,
+          MATKL3: "非金属建筑材料",
+          CODE: "09",
+          SALK3: 1000000
+        },
+        {
+          id: 10,
+          MATKL3: "水泥及制品",
+          CODE: "10",
           SALK3: 1000000
         }
       ]
@@ -99,8 +216,19 @@ export default {
       return "";
     },
     handleSizeChange() {},
-    handleCurrentChange() {}
-  }
+    handleCurrentChange() {},
+    createRandomData() {
+      let arr = [
+        "中心库",
+        "转运库",
+        "专用管分公司",
+        "原炼化分公司",
+        "港东器材库",
+        "辅助器材库"
+      ];
+    }
+  },
+  mounted() {}
 };
 </script>
 
