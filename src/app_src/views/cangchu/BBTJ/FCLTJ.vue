565bef39017fe41bc8ec725ecc9949b8600b957c
zpjune <1084013577@qq.com>
2019-11-12 11:07:00 +0800

    sd

--- src/app_src/views/cangchu/BBTJ/FCLTJ.vue
+++ src/app_src/views/cangchu/BBTJ/FCLTJ.vue
@@ -5,20 +5,11 @@
         <el-col :span="24">
           <el-date-picker
             size="mini"
-            v-model="listSumQuery.B_BUDAT"
+            v-model="listQuery.mont"
             style="width:200px;"
             type="month"
             value-format="yyyy-MM-dd"
             placeholder="选择年月(起)"
-            :clearable="true"
-          ></el-date-picker>
-          <el-date-picker
-            size="mini"
-            v-model="listSumQuery.E_BUDAT"
-            style="width:200px;"
-            type="month"
-            value-format="yyyy-MM-dd"
-            placeholder="选择年月(止)"
             :clearable="true"
           ></el-date-picker>
           <el-button
@@ -27,7 +18,7 @@
             type="primary"
             icon="el-icon-search"
             @click="handleFilter"
-          >搜索</el-button>
+          >查询</el-button>
           <!-- <el-button size="mini" type="primary" icon="el-icon-edit" @click="exportSumExcel">导出</el-button> -->
         </el-col>
       </el-row>
@@ -36,9 +27,8 @@
     <el-card class="box-card">
       <el-table
         size="mini"
-        :data="sumlist"
+        :data="list"
         :header-cell-class-name="tableRowClassName"
-        v-loading="sumlistLoading"
         element-loading-text="给我一点时间"
         border
         fit
@@ -47,17 +37,6 @@
         :summary-method="getSummaries"
         show-summary
       >
-        <!-- <el-table-column
-          width="180px"
-          fixed="left"
-          align="center"
-          label="单位"
-          :show-overflow-tooltip="true"
-        >
-          <template slot-scope="scope">
-            <span>{{scope.row.S_OrgName}}</span>
-          </template>
-        </el-table-column>-->
         <el-table-column width="120px" align="center" label="时间月">
           <template slot-scope="scope">
             <span>{{scope.row.BUDAT|parseTime1}}</span>
@@ -91,92 +70,6 @@
         </el-table-column> -->
       </el-table>
     </el-card>
-    <el-dialog :visible.sync="showed" width="50%" title="月度明细">
-      <div class="topSearh">
-        <el-row>
-          <el-col :span="24">
-            <el-input
-              style="width: 200px;"
-              class="filter-item"
-              placeholder="出库通知单号"
-              v-model="listQuery.ZCKTZD"
-              size="mini"
-            ></el-input>
-            <el-input
-              style="width: 200px;"
-              class="filter-item"
-              placeholder="物料"
-              v-model="listQuery.MATNR"
-              size="mini"
-            ></el-input>
-            <el-button
-              size="mini"
-              class="filter-item"
-              type="primary"
-              icon="el-icon-search"
-              @click="handleFilterDetail"
-            >搜索</el-button>
-          </el-col>
-        </el-row>
-      </div>
-      <el-card class="box-card">
-        <el-table
-          size="mini"
-          :data="list"
-          :header-cell-class-name="tableRowClassName"
-          v-loading="listLoading"
-          element-loading-text="给我一点时间"
-          border
-          fit
-          highlight-current-row
-          style="width: 100%"
-        >
-          <el-table-column width="180px" align="center" label="发出日期">
-            <template slot-scope="scope">
-              <span>{{scope.row.BUDAT|parseTime}}</span>
-            </template>
-          </el-table-column>
-          <el-table-column width="180px" align="center" label="出库通知单号">
-            <template slot-scope="scope">
-              <span>{{scope.row.ZCKTZD}}</span>
-            </template>
-          </el-table-column>
-          <el-table-column width="100px" align="center" label="发出量">
-            <template slot-scope="scope">
-              <span>{{scope.row.ZGZSL}}</span>
-            </template>
-          </el-table-column>
-          <el-table-column width="100px" align="center" label="金额">
-            <template slot-scope="scope">
-              <span>{{scope.row.DMBTR|NumFormat}}</span>
-            </template>
-          </el-table-column>
-
-          <el-table-column width="180px" align="right" label="物料">
-            <template slot-scope="scope">
-              <span>{{scope.row.MATNR }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column align="right" label="库存地点" :show-overflow-tooltip="true">
-            <template slot-scope="scope">
-              <span>{{scope.row.LGORT }}</span>
-            </template>
-          </el-table-column>、
-        </el-table>
-        <div class="page">
-          <el-pagination
-            background
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            :current-page="listQuery.page"
-            :page-sizes="[10,20,30, 50]"
-            :page-size="listQuery.limit"
-            layout="total, sizes, prev, pager, next, jumper"
-            :total="total"
-          ></el-pagination>
-        </div>
-      </el-card>
-    </el-dialog>
   </div>
 </template>
 
@@ -189,116 +82,14 @@ export default {
   name: "FCLTJ",
   data() {
     return {
-      showed: false,
       tableKey: 0,
       list: [],
-      sumlist: [
-          {
-          BUDAT: "2019-01-08 12:00:00",
-          ZGZSL: 900,
-          GC:'港东供销',
-          MSEG: 210000
-        },
-          {
-          BUDAT: "2019-02-08 12:00:00",
-          ZGZSL: 510,
-           GC:'港西供销',
-          MSEG: 210000
-        },
-          {
-          BUDAT: "2019-03-08 12:00:00",
-          ZGZSL: 560,
-           GC:'港狮供销',
-          MSEG: 280000
-        },
-          {
-          BUDAT: "2019-04-08 12:00:00",
-          ZGZSL: 300,
-           GC:'油区供销',
-          MSEG: 210000
-        },
-        {
-          BUDAT: "2019-05-08 12:00:00",
-          ZGZSL: 500,
-           GC:'港骅供销',
-          MSEG: 230000
-        }
-      ],
-      sumlistLoading: false,
-      list: [
-        {
-          ZCKTZD:"2-20170203-00032",
-          BUDAT: "2019/01/31 00:00:00.000",
-          ZGZSL: 76,
-          DMBTR: 35000,
-          MATNR:"物料1",
-          LGORT:"仓库1"
-        },
-         {
-          ZCKTZD:"3-20191008-00033",
-          BUDAT: "2019-10-08 12:00:00",
-          ZGZSL: 57,
-          DMBTR: 35000,
-          MATNR:"物料1",
-          LGORT:"仓库1"
-        },
-         {
-          ZCKTZD:"5-20191008-00035",
-          BUDAT: "2019-10-08 12:00:00",
-          ZGZSL: 37,
-          DMBTR: 35000,
-          MATNR:"物料1",
-          LGORT:"仓库1"
-        },
-         {
-          ZCKTZD:"2-20191008-00011",
-          BUDAT: "2019-10-08 12:00:00",
-          ZGZSL: 97,
-          DMBTR: 35000,
-          MATNR:"物料1",
-          LGORT:"仓库1"
-        },
-         {
-          ZCKTZD:"4-20191008-00031",
-          BUDAT: "2019-10-08 12:00:00",
-          ZGZSL: 107,
-          DMBTR: 35000,
-          MATNR:"物料1",
-          LGORT:"仓库1"
-        },
-         {
-          ZCKTZD:"4-20191008-00051",
-          BUDAT: "2019-10-08 12:00:00",
-          ZGZSL: 77,
-          DMBTR: 35000,
-          MATNR:"物料1",
-          LGORT:"仓库1"
-        }
-      ],
-      total: null,
-      totalDetail: null,
-      listLoading: false,
-      listSumQuery: {
-        B_BUDAT: "",
-        E_BUDAT: ""
-        // S_OrgCode: this.$store.state.user.orgCode,
-        // S_WorkDate: this.$store.state.user.sysTime,
-        // S_OrgName: this.$store.state.user.departName,
-        //TaxNumber: ""
-      },
+      total: 0,
       listQuery: {
         page: 1,
         limit: 10,
-        //Is_Page: "true",
-        ZCKTZD: "",
-        MATNR: ""
-        // S_OrgCode: this.$store.state.user.orgCode,
-        // // S_BeginWorkDate: '',
-        // // S_EndWorkDate: this.$store.state.user.sysTime,
-        // S_OrgName: this.$store.state.user.departName,
-        // S_WorkDate: this.$store.state.user.sysTime,
-        // TaxNumber: "",
-        // TaxRate: ""
+        month: "",
+        WERKS_NAME:''
       }
     };
   },
@@ -308,132 +99,19 @@ export default {
   },
 
   methods: {
-    getSummaries(param) {
-      const { columns, data } = param;
-      const sums = [];
-      columns.forEach((column, index) => {
-        if (index === 0) {
-          sums[index] = "合计";
-          return;
-        }
-        const values = data.map(item => item[column.property]);
-        if (!values.every(value => isNaN(value))) {
-          sums[index] = values.reduce((prev, curr) => {
-            const value = Number(curr);
-            if (!isNaN(value)) {
-              return prev + curr;
-            } else {
-              return prev;
-            }
-          }, 0);
-          if (index === 2) {
-            sums[index] += " 元";
-          } else {
-            sums[index] = sums[index].toFixed(2);
-            sums[index] += " 吨";
-          }
-        } else {
-          sums[index] = "";
-        }
-      });
+    handleFilter(){
 
-      return sums;
-    },
-    getSumList() {
-      //  this.sumlistLoading = true;
-      //   getGroupSumMonthTaxSalary(this.listSumQuery).then(response => {
-      //     if (response.data.code === 2000) {
-      //       this.sumlist = response.data.items;
-      this.sumlistLoading = false;
-      //     } else {
-      //       this.$notify({
-      //         position: "bottom-right",
-      //         title: "失败",
-      //         message: response.data.message,
-      //         type: "error",
-      //         duration: 2000
-      //       });
-      //     }
-      //   });
-    },
-    sumdetail(orgName, orgCode) {
-      this.title = "月度明细";
-      this.showed = true;
-      //   this.listQuery.S_OrgCode = orgCode;
-      //   this.listQuery.S_OrgName = orgName;
-      //   this.listQuery.S_WorkDate = this.listSumQuery.S_WorkDate;
-      //   this.listQuery.S_Department = this.listSumQuery.S_Department;
-      this.getList();
     },
 
-    getList() {
-      //   this.listLoading = true;
-      //   getMonthTaxSalary(this.listQuery).then(response => {
-      //     if (response.data.code === 2000) {
-      //this.list = response.data.items;
-      this.total = 15;
-      this.listLoading = false;
-      // } else {
-      //   this.listLoading = false;
-      //   this.$notify({
-      //     position: "bottom-right",
-      //     title: "失败",
-      //     message: response.data.message,
-      //     type: "error",
-      //     duration: 2000
-      //   });
-      // }
-      //});
-    },
-    created() {
-      this.getSumList();
-      //this.getList();
-      //this.createTime();
-    },
-    handleSizeChange(val) {
-      this.listQuery.limit = val;
-      this.getList();
-    },
-    handleCurrentChange(val) {
-      this.listQuery.page = val;
-      this.getList();
-    },
-    formatJson(filterVal, jsonData) {
-      return jsonData.map(v =>
-        filterVal.map(j => {
-          return v[j];
-        })
-      );
-    },
-    handleFilter() {
-      //this.listQuery.page = 1;
-      this.getSumList();
-    },
+   
 
-    handleFilterDetail() {
-      this.listQuery.page = 1;
-      this.getList();
-    },
     tableRowClassName({ row, rowIndex }) {
       if (rowIndex === 0) {
         return "el-button--primary is-active"; // 'warning-row'
       } // 'el-button--primary is-plain'// 'warning-row'
       return "";
-    }
-  },
-  computed: {
-    getRoleLevel() {
-      if (this.$store.state.user.roleLevel === "admin") {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    headers() {
-      return {
-        "X-Token": getToken()
-      };
-    }
+  
+  }
   }
 };
 </script>
